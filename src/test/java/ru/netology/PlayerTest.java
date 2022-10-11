3d09f3546079f6e3707f60392b33035077eaf398
LiubovSokolova89 <liubow.sokolova@gmail.com>
2022-10-11 12:24:59 +0200

    adding tests

--- src/test/java/ru/netology/PlayerTest.java
+++ src/test/java/ru/netology/PlayerTest.java
@@ -1,6 +1,8 @@
 package ru.netology;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class PlayerTest {
@@ -19,5 +21,54 @@ public class PlayerTest {
         assertEquals(expected, actual);
     }
 
+    @Test
+    public void shouldGetMostPlayerByGenre(){
+        GameStore store = new GameStore();
+        Game game1 = store.publishGame("Cars", "Гонки");
+        Game game2 = store.publishGame("Race", "Гонки");
+
+        Player player = new Player("Bill");
+        player.installGame(game1);
+        player.installGame(game2);
+
+        player.play(game1, 5);
+
+        Game expected = null;
+        Game actual = player.mostPlayerByGenre(game2.getGenre());
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldCalcIfSameGenreGames() {
+        GameStore store = new GameStore();
+        Game game1 = store.publishGame("Cars", "Гонки");
+        Game game2 = store.publishGame("Race", "Гонки");
+
+        Player player = new Player("Bill");
+        player.installGame(game1);
+        player.installGame(game2);
+        player.play(game1, 5);
+        player.play(game2, 10);
+
+        int expected = 15;
+        int actual = player.sumGenre("Гонки");
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldCheckGamesIfThereAreExceptions() {
+        GameStore store = new GameStore();
+        Game game1 = store.publishGame("Cars", "Гонки");
+        Game game2 = store.publishGame("Race", "Гонки");
+
+        Player player = new Player("Bill");
+        player.installGame(game1);
+        player.play(game1, 15);
+
+        Assertions.assertThrows(RuntimeException.class, () -> player.sumGenre("Гонки"));
+    }
+    
     // другие ваши тесты
 }
